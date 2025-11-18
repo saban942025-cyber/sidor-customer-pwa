d06fd362b721805ca8234c9cf38ea15eda1a6e9f
saban942025-cyber <saban942025@gmail.com>
2025-11-18 13:17:23 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx' // ודא שהקובץ נקרא בדיוק App.jsx (עם A גדולה)
-import './index.css' 
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
