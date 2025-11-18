6df0b9da076f135be4a9f56c6baa5a4e5f739ac2
saban942025-cyber <saban942025@gmail.com>
2025-11-18 14:44:57 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // Vite מוציא ל-dist כברירת מחדל, אבל אפשר להוסיף את זה ליתר ביטחון:
   build: {
-    outDir: 'dist', // וודא שזה מוגדר ככה
+    outDir: 'dist',
   }
 })
